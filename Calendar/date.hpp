5c22d0f3a34c08707adde82ef93f1e017d558936
cholewadamian1997 <cholewa.damian1997@gmail.com>
2017-10-18 10:50:27 +0200

    Update date.hpp

--- Calendar/date.hpp
+++ Calendar/date.hpp
@@ -14,14 +14,14 @@ class Date{
 	public:
 
 	Date(int _day = 1, int _month = 1, int _year = 1970);
-	Date (const Date & date);
-	showDate();
-    	Date operator+(int days);
-    	Date operator-(int days);
-    	Date operator-(const Date &date);
-    	bool operator==(const Date &date);
-    	bool operator!=(const Date &date);
-    	friend std::ostream &operator<<(ostream &out, const Date &date);
+	Date (const Date & date); //Szymon
+	showDate(); //Szymon
+    	Date operator+(int days); //Asia
+    	Date operator-(int days); //Asia
+    	Date operator-(const Date &date); //Asia
+    	bool operator==(const Date &date); //Damian
+    	bool operator!=(const Date &date); //Damian
+    	friend std::ostream &operator<<(ostream &out, const Date &date); //Damian
 
 };
 #endif /* DATA_HPP_ */
