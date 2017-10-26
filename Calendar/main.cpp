ed7c018fd88549be2c0bd46076822a46a32fdbb8
cholewadamian1997 <cholewa.damian1997@gmail.com>
2017-10-27 00:50:13 +0200

    Update main.cpp

--- Calendar/main.cpp
+++ Calendar/main.cpp
@@ -1,11 +1,24 @@
-#include "data.hpp"
+#include "Date.hpp"
 
-
-using namespace std;
 
 int main(){
-	Date data1;
-	cout << data1;
+
+   Date date1(25,3,2012);
+   cout << "date1: " << date1 << endl;
+
+   Date date2 = date1 + 367;
+   cout << "date2: " << date2 << endl;
+
+
+   int difference = date2 - date1;
+   cout << "date2 - date1: " << difference << endl;
+
+   if(date1 == date2) {cout << "date1 = date2" << endl;}
+   else cout << "date1 != date2" << endl;
+
+   int days = 4382;
+   date1 -= days;
+   cout << "date " << days <<" before date1: " << date1 << endl;
 
 
 return 0;
