e103e5ffa30a20915c8719e217334e3fa012d61d
cholewadamian1997 <cholewa.damian1997@gmail.com>
2018-01-24 08:20:44 +0100

    Update Factory.cpp

--- Factory.cpp
+++ Factory.cpp
@@ -10,9 +10,9 @@ std::map<IReceiver*, double > Preferences::getPreferences(){
 void Preferences::setPreferences(std::map<IReceiver* , double> preferences){
 
     double tmp;
-    for (auto it=std::begin(preferences); it!=std::end(preferences); ++it){
+    for (auto &it : preferences){
         std::cin >> tmp;
-        it->second = tmp;
+        it.second = tmp;
     }
 }
 
@@ -20,30 +20,30 @@ void Preferences::addReceiver(IReceiver* receiver){
 
     preferences.insert (std::pair<IReceiver*, double>(receiver, (1/preferences.size())));
 
-    for (auto it=std::begin(preferences); it!=std::end(preferences); ++it)
-        it->second=(1/preferences.size());
+    for (auto &it : preferences)
+        it.second=(1/preferences.size());
 }
 
 void Preferences::addReceiverWithPreferences(IReceiver* receiver, double probability){
 
-    for (auto it=std::begin(preferences); it!=std::end(preferences); ++it)
-        it->second=(1-probability)*(it->second);
+    for (auto &it : preferences)
+        it.second=(1-probability)*(it.second);
 
     preferences.insert (std::pair<IReceiver*, double>(receiver, probability));
 }
 
 void Preferences::removeReceiver(IReceiver* receiver){
 
-    for (auto it=std::begin(preferences); it!=std::end(preferences); ++it)
-        it->second=(it->second)/(1-preferences[receiver]);
+    for (auto &it : preferences)
+        it.second=(it.second)/(1-preferences[receiver]);
 
     preferences.erase(receiver);
 }
 
-std::pair<IReceiver*, double> Preferences::view(){
+void Preferences::view(){
 
-    for (auto it=std::begin(preferences); it!=std::end(preferences); ++it)
-        std::cout << "ID: " << it->first << "  Probability: " << it->second << std::endl;
+    for (auto &it : preferences)
+        std::cout << "ID: " << it.first << "  Probability: " << it.second << std::endl;
 }
 
 IReceiver* Preferences::drawReceiver(){
@@ -53,18 +53,18 @@ IReceiver* Preferences::drawReceiver(){
     int digits_after_dot;
     int max_size = 0;
 
-    for(auto it=std::begin(preferences); it!=std::end(preferences); ++it){
+    for(auto &it : preferences){
 
-        ss << it->second;
+        ss << it.second;
         str = ss.str();
 
-        if (it->second > 1){
+        if (it.second > 1){
             std::cout << "EROR" << std::endl;
             digits_after_dot = -1;
         }
-        if (it->second == 0)
+        if (it.second == 0)
             digits_after_dot = -1;
-        if (it->second == 1)
+        if (it.second == 1)
             digits_after_dot = 0;
         else
             digits_after_dot = str.size()-2;
@@ -81,13 +81,109 @@ IReceiver* Preferences::drawReceiver(){
 
     int x = 0;
 
-    for (auto it=std::begin(preferences); it!=std::end(preferences); ++it){
+    for (auto &it : preferences) {
 
-        if (x <= drawn_number < x + it->second)
-            return it->first;
+        if (x <= drawn_number &&  drawn_number < x + it.second)
+            return it.first;
 
-        x += it->second;
+        x += it.second;
     }
 }
+// class: Product
+Product::Product(ElementID _id) {id = _id;}
 
-// class: IStorage
+Product::Product(const Product& product) {id = product.id;}
+
+ElementID Product::getId() { return id;}
+
+// class: Storage
+void Storage::push(const Product& product){ products_deque.push_back(product); }
+
+//true if the Storage size is 0, false otherwise.
+bool Storage::isEmpty() { return products_deque.empty(); }
+
+int Storage::storage_size() { return products_deque.size(); }
+
+void Storage::view() {
+
+    for (auto &it : products_deque)
+        std::cout << it.getId() << std::endl;
+}
+
+// class: Queue
+QueueType Queue::getQueueType() {return queue_type;}
+
+void Queue::push(const Product& product) { products_deque.push_back(product); }
+
+void Queue::pop(){
+
+    if (queue_type == FIFO)
+        products_deque.pop_back();
+
+    else if (queue_type == LIFO)
+        products_deque.pop_front();
+}
+
+bool Queue::isEmpty() {return products_deque.empty();}
+
+int Queue::queueSize() {return products_deque.size();}
+
+void Queue::view() {
+
+    for (auto &it : products_deque)
+        std::cout << it.getId() << std::endl;
+}
+
+// class: Sender
+Sender::Sender(Preferences _receiver_preferences) {
+    receiver_preferences = _receiver_preferences;
+}
+
+void Sender::sendProduct(){
+    if (list_of_products.size() == 0){
+        std::cout << "Brak produktów do wysłania";
+    }
+    Product product_to_send = list_of_products[0];
+    std::vector<Product>:: iterator it = list_of_products.begin();
+    list_of_products.erase(it);
+    auto chosen_receiver = receiver_preferences.drawReceiver();
+    //chosen_receiver.receiveProduct(product_to_send);
+}
+
+void Sender::setPreferences(Preferences pref){
+    receiver_preferences = pref;
+}
+
+Preferences Sender::getPreferences(){
+    return receiver_preferences;
+}
+
+// class StorageHouse
+Storehouse::Storehouse(ElementID _id) {id = _id; }
+
+void Storehouse::receiveProduct(Product product) {
+    storage->push(product);
+}
+
+void Storehouse::viewDepot() {
+    storage->view();
+}
+
+ElementID Storehouse::getId() {
+    return id;
+}
+
+ReceiverType Storehouse::getReceiverType() {
+    return STOREHOUSE;
+}
+
+// class Ramp
+ElementID Ramp::getId() {
+    return id;
+}
+
+Duration Ramp::getDeliveryFrequency() {
+    return delivery_frequency;
+}
+
+//
